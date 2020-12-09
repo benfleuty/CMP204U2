51e36dce0dc5e4e17228c42fa8663b48373f831f
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-09 16:51:27 +0000

    post dummy comments

--- comments.php
+++ comments.php
@@ -1,42 +1,67 @@
 <?php
+session_start();
+require_once "config.php";
 /** Parent variable definitions
  *
  * @var $commentTarget
+ * @var $db
  *
  */
 
 
-$sql = `SELECT CMP204comments.content, CMP204comments.commentedOn, CMP204users.username FROM CMP204comments, CMP204users
-        WHERE CMP204comments.userId = CMP204users.id AND CMP204comments.target = ${commentTarget}}`;
+$output = '<div id="comments">';
 
-?>
-<div class="comments">
-    <div class="comment-outer">
-        <div class="comment-body">
-            <p>Veniam tempor quis sit magna commodo. Aliquip Lorem ut labore voluptate nostrud officia
-                excepteur!</p>
-        </div>
-        <hr class="comment-hr">
-        <div class="comment-footer">
-            <div class="comment-user-details">
-                <p>username date time</p>
-            </div>
-            <div class="comment-user-controls">
-                <i class="fa fa-edit"></i>
-                <i class="fa fa-trash" aria-hidden="true"></i>
-            </div>
-        </div>
-    </div>
-    <div class="comment-outer">
+if (loggedIn()) {
+    $output .= '<div class="comment-outer">
         <div class="comment-body">
             <label for="commentText" content="Your comment:">
-                <textarea id="commentText" aria-multiline="true" maxlength="140" placeholder="Comment..." required form="commentForm"></textarea>
+                    <textarea id="commentText" aria-multiline="true" maxlength="140" placeholder="Comment..." required
+                              form="commentForm"></textarea>
             </label>
         </div>
         <hr class="comment-hr">
         <div class="comment-footer">
-            <button id="postCommentButton" class="bg-primary text-white" type="submit" form="commentForm">Post</button>
-            <form id="commentForm" ></form>
+            <button id="postCommentButton" class="bg-primary text-white" type="submit" form="commentForm">Post
+            </button>
+            <form id="commentForm"></form>
         </div>
     </div>
-</div>
+    ';
+}
+
+if (!loggedIn()){
+    $output .= '<div class="comment-outer comment-not-logged-in-message">
+        <div class="comment-body">
+            <p>You must be logged in to comment</p>
+        </div>
+    </div>';
+}
+
+$stmt = $db->stmt_init();
+$sql = "SELECT CMP204comments.content, CMP204comments.date, CMP204users.username FROM CMP204comments, CMP204users
+        WHERE CMP204comments.userId = CMP204users.id AND CMP204comments.target = ?";
+if (!$stmt = $db->prepare($sql)) errorOutWithMessage("GetCustomers could not prepare SQL statement");
+$stmt->bind_param("s",$commentTarget);
+$stmt->execute();
+$result = $stmt->get_result(); // get the query result
+
+$output .= '<div id="comments-output">';
+
+if (mysqli_num_rows($result) <= 0) {
+    $output .= '<div class="comment-outer no-comments-message"><div class="comment-body"><p>There are no comments!</p></div></div>';
+}
+foreach ($result as $row) {
+    $output .= '<div class="comment-outer"><div class="comment-body">'.
+                "<p>{$row["content"]}</p>".
+        '</div>
+        <hr class="comment-hr">
+        <div class="comment-footer">
+        <div class="comment-user-details">'.
+        "<p>{$row["username"]} {$row["date"]}</p></div>";
+
+    if ($_SESSION["username"] == $row["username"])
+        $output .= '<div class="comment-user-controls"><i class="fa fa-edit"></i><i class="fa fa-trash" aria-hidden="true"></i></div>';
+}
+$output .= '</div>';
+
+echo $output;
--- postComment.php
+++ postComment.php
@@ -1,1 +1,17 @@
-<?php
+<?php session_start();
+include "config.php";
+if (!loggedIn()) return;
+
+$comment = $_POST["comment"];
+
+
+
+
+// delete
+$result = json_encode(array("success", "namee", "idee", "datee"));
+echo $result;
+//$result = array("success", "namee", "idee", "datee");
+///////////////////
+// echo "successText,username,commentId";
+//echo $result;
+//    echo "success,{$_SESSION["username"]},{$result["id"]},{$result["date"]}";
