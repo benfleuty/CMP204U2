5ddf3851a6124d5b01c33dd99875d9d31d9a3a38
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-11 23:37:15 +0000

    fix typos

--- js/privacy.js
+++ js/privacy.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
     $(document).on("click", "#delete-account", function () {
+        console.info("click");
         let delAcc = confirm("Your account will be deleted!");
         if (!delAcc) return;
-        $("#delete-account").click(function (e) {
-            e.preventDefault();
-            $.ajax({
-                url: "deleteAccount.php",
-                method: "POST",
-                success: function (data) {
-                    if (data === "success") {
-                        alert("Your account was deleted");
-                        $.ajax({
-                            url: "logout.php",
-                            method: "POST",
-                            success: function () {
-                                location.replace("index.php");
-                            }
-                        });
-                    } else if (data === "guest")
-                        alert("You are not logged in!");
-                    else alert("There was an error. Your account was not deleted!");
-                }
-            });
+        console.info("delacc: " + delAcc);
+        $.ajax({
+            url: "deleteAccount.php",
+            method: "POST",
+            success: function (data) {
+                console.info("data: " + data);
+                if (data === "success") {
+                    alert("Your account was deleted");
+                    $.ajax({
+                        url: "logout.php",
+                        method: "POST",
+                        success: function () {
+                            location.replace("index.php");
+                        }
+                    });
+                } else if (data === "guest")
+                    alert("You are not logged in!");
+                else alert("There was an error. Your account was not deleted!");
+            }
         });
     });
 })
