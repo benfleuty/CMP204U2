618dc4c64443aea8fa80c3546f29d9d146d9a9a5
Ben Fleuty <22148138+benfleuty@users.noreply.github.com>
2020-12-14 11:36:00 +0000

    add cookie plugin
    https://www.jqueryscript.net/other/GDPR-Cookie-Consent-Popup-Plugin.html

--- script-links.php
+++ script-links.php
@@ -4,3 +4,37 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="js/comments.js"></script>
 <script src="js/privacy.js"></script>
+<script src="js/jquery.ihavecookies.min.js"></script>
+<script type="text/javascript">
+    var options = {
+        title: '&#x1F36A; Accept Cookies & Privacy Policy?',
+        message: 'We only use essential cookies to give you the best experience! None of that ad tracking stuff.',
+        delay: 600,
+        expires: 1,
+        link: '#privacy',
+        onAccept: function(){
+            var myPreferences = $.fn.ihavecookies.cookie();
+            console.log('Yay! The following preferences were saved...');
+            console.log(myPreferences);
+        },
+        uncheckBoxes: true,
+        acceptBtnLabel: 'Accept Cookies',
+        moreInfoLabel: 'More information',
+        cookieTypesTitle: 'Select which cookies you want to accept',
+        fixedCookieTypeLabel: 'Essential',
+        fixedCookieTypeDesc: 'These are essential for the website to work correctly.'
+    }
+
+    $(document).ready(function() {
+        $('body').ihavecookies(options);
+
+        if ($.fn.ihavecookies.preference('marketing') === true) {
+            console.log('This should run because marketing is accepted.');
+        }
+
+        $('#ihavecookiesBtn').on('click', function(){
+            $('body').ihavecookies(options, 'reinit');
+        });
+    });
+
+</script>
